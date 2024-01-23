746a6d056dca37524db2f3e67214d2b9d545d390
Joris Lasserre <joris.lasserre@iut.u-bordeaux.fr>
2024-01-23 18:30:16 +0100

    first commit

---
+++ spotify.js
@@ -0,0 +1,19 @@
+var client_id = '1ea1212d3c9f42f286ba2ca976454aba';
+var client_secret = 'a3a254795503413e8b207d924dca18ea';
+
+var authOptions = {
+  url: 'https://accounts.spotify.com/api/token',
+  headers: {
+    'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64'))
+  },
+  form: {
+    grant_type: 'client_credentials'
+  },
+  json: true
+};
+
+request.post(authOptions, function(error, response, body) {
+  if (!error && response.statusCode === 200) {
+    var token = body.access_token;
+  }
+});
